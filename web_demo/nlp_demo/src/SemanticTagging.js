27e7072f43426b48ee3d874583bb0a9e1d93ef76
Andrew Moore <andrew.p.moore94@gmail.com>
2021-06-09 11:34:06 +0100

    Semantic tagging and UCREL pages

---
+++ web_demo/nlp_demo/src/SemanticTagging.js
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+
+import UCRELDoc from './UCRELDoc';
+
+async function getJSONData(url = '') {
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'same-origin'
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    else {
+        if (response.headers.has('Content-Type')){
+            if (response.headers.get('Content-Type').includes('application/json')){
+                return response.json(); 
+            }
+            
+        }
+        throw new Error(`Wrong Content-Type Return`); 
+    }
+}
+
+const UCRELSpinner = (props) => {
+    if (Object.keys(props.data).length === 0){
+        return (
+            <Container className="flex flex-center pt-3">
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+            </Container>
+        )
+    }
+    else if ('error' in props.data){
+        return(<h5>Error: Example document not in the correct location.</h5>)
+    }
+    else {
+        return(<UCRELDoc tokens={props.data.tokens} sentenceIndexes={props.data.sentence_indexes}/>);
+    }
+}
+
+function KeyBoxes(props) {
+    return (
+        <span>
+            <span className="key-icon">
+                <svg viewBox="0 0 10 10" id={props._id}>
+                    <rect x="0" y="0" width="100%" height="100%"/>
+                </svg>
+            </span>
+            <span className="pl-2 key-text">{props._key}</span>
+        </span>
+    )
+}
+
+function SemanticTagKey() {
+    // For the CSS tags see the `_ucrel.scss` file.
+    const css_id_to_usas_tag = {"E1": "Emotion", "N3": "Measurement", 
+                                "Z1": "Name", "N1": "Number", "T1": "Time"};
+    const keyBoxes = [];
+    for (let css_id in css_id_to_usas_tag){
+        const usas_tag = css_id_to_usas_tag[css_id];
+        keyBoxes.push(<KeyBoxes  _id={css_id} _key={usas_tag} key={css_id} />);
+    }
+
+    return (
+        
+        <div className="pb-2">
+            <h4 className="pt-2 pb-1 center-text"> Semantic Tag Key </h4>
+            <div className="flex-wrap flex-space-evenly">
+                {keyBoxes}
+            </div>
+        </div>
+    )
+}
+
+function SemanticTagging() {
+    const [ucrelData, setUcrelData] = useState({})
+    
+    useEffect( () => {
+        if (Object.keys(ucrelData).length === 0){
+            getJSONData(process.env.PUBLIC_URL + '/data/usas_example.json')
+            .then(setUcrelData)
+            .catch(() => {setUcrelData({'error': true})});
+        }
+    }, [ucrelData])
+
+    
+
+    return(
+        <Container className="sematic-tagging-container full-height" id="full-width">
+            <h1>Semantic Tagging</h1>
+            <p className="lead text-muted">
+                The task of predicting the broad dictionary sense for 
+                each word in a given text.
+            </p>
+            <hr />
+            <SemanticTagKey/>
+            <hr />
+            <UCRELSpinner data={ucrelData} />
+
+            
+        </Container>
+    )
+} 
+
+export default SemanticTagging; 
---
+++ web_demo/nlp_demo/src/UCRELDoc.js
@@ -0,0 +1,63 @@
+import Container from 'react-bootstrap/Container';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
+
+
+const UCRELToken = (props) => {
+    let text = props.token.text;
+    let tooltipData = [];
+    let _id = "";
+    // The USAS label is converted from an N length sequence e.g. T1.2 to a 
+    // string that is of length 2 e.g. T1.2 will become T1. This was done so 
+    // that we can match on a higher level category for the CSS styling. 
+    if ("usas_label" in props.token){
+        tooltipData.push(<p key={"usas"}>Semantic label = {props.token.usas_label}</p>);
+        _id = props.token.usas_tag.slice(0,2)
+    }
+    if ("pos_label" in props.token){
+        tooltipData.push(<p key={"pos"}>Part Of Speech = {props.token.pos_label}</p>);
+    }
+    return (
+        <OverlayTrigger placement="top" overlay={<Tooltip>{tooltipData}</Tooltip>}>
+            <mark className="ucrel-token shadow-sm" id={_id} >
+                {text}
+            </mark>
+        </OverlayTrigger>
+    )
+    
+}
+
+const UCRELSentence = (props) => {
+    return (
+        <div  className="py-3 flex-wrap flex-start">
+            {props.tokens}
+        </div>
+    )
+}
+
+
+const UCRELDoc = (props) => {
+    
+    const sentenceIndexes = props.sentenceIndexes;
+    const tokens = props.tokens
+
+    // Creates a UCRELSentence object per sentence of tokens
+    const sentences = [];
+    for (let index of sentenceIndexes) {
+        const [startIndex, endIndex] = index;
+        const sentenceTokens = [];
+        for (let tokenIndex = startIndex; tokenIndex < endIndex; tokenIndex++) {
+            sentenceTokens.push(<UCRELToken key={tokenIndex} token={tokens[tokenIndex]}></UCRELToken>);
+        }
+        sentences.push(<UCRELSentence tokens={sentenceTokens} key={index}/>);
+    }
+    
+    return (
+        <Container>
+                {sentences}
+        </Container>
+    )
+}
+
+export default UCRELDoc
